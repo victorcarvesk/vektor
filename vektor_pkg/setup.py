86c0195ca429a6963f41e3d859f0051af1dc6065
Victor Carvalho <victorcarvesk@gmail.com>
2023-06-14 02:13:16 -0300

    fix subdirectories copy and insert models

--- vektor_pkg/setup.py
+++ vektor_pkg/setup.py
@@ -7,10 +7,35 @@ package_name = 'vektor_pkg'
 package_folders = []
 package_folders.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
 package_folders.append(('share/' + package_name, ['package.xml']))
-package_folders.append((os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))))
-package_folders.append((os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.[ws][od]*'))))
-package_folders.append((os.path.join('share', package_name, 'description'), glob(os.path.join('description', '*.xacro'))))
-package_folders.append((os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.rviz'))))
+# package_folders.append((os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))))
+# package_folders.append((os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.*'))))
+# package_folders.append((os.path.join('share', package_name, 'description'), glob(os.path.join('description', '*.xacro'))))
+# package_folders.append((os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.rviz'))))
+
+folders = ['worlds', 'launch', 'description', 'config']
+
+for folder in folders:
+
+    files = glob(os.path.join((folder + '/**'), '*.*') , recursive=True)
+    for file in files:
+        package_folders.append((os.path.join('share', package_name, os.path.dirname(file)), [file]))
+
+
+def package_files(data_files, directory_list):
+
+    paths_dict = {}
+    for directory in directory_list:
+        for (path, directories, filenames) in os.walk(directory):
+            for filename in filenames:
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join('share', package_name, path)
+                if install_path in paths_dict.keys():
+                    paths_dict[install_path].append(file_path)
+                else:
+                    paths_dict[install_path] = [file_path]
+    for key in paths_dict.keys():
+        data_files.append((key, paths_dict[key]))
+    return data_files
 
 setup(
     name=package_name,
